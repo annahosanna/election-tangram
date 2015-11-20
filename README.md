85d95475ca7fd10362fd8ad57b3a2a7889d56581
Nick Doiron <nicholas.doiron@asiafoundation.org>
2015-11-20 11:00:29 -0800

    include more parties, add labels

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Election-Tangram
 
-test map
+Fork of Nathaniel Kelso's <a href="https://github.com/tangrams/cinnabar-style-more-labels">Cinnabar</a> map style and visualization.
+
+Displays basemap with Burmese labels, using Google Noto Myanmar font
+
+Overlays 2015 election results GeoJSON over land area, but beneath labels
+
+## License
+
+Open source under an MIT license
--- result-loader.js
+++ result-loader.js
@@ -5,7 +5,7 @@ var candidates = require('./data/final_lower_house.json');
 
 for (var f = 0; f < lowerhouse.features.length; f++) {
   for (var c = 0; c < candidates.length; c++) {
-	  if (candidates[c].votes && candidates[c].constituency.TS_PCODE === lowerhouse.features[f].properties.TS_PCODE) {
+	  if (candidates[c].votes > 0 && candidates[c].constituency.TS_PCODE === lowerhouse.features[f].properties.TS_PCODE) {
 		  lowerhouse.features[f].properties.winner = candidates[c].party_id || -1
 		  //console.log('found winner');
 	  }
